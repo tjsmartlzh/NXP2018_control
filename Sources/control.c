7c387dfd6765d2a64e6408f93827f4cc32a4f56c
tjsmartlzh <lizihao20081@163.com>
2018-04-18 21:13:10 +0800

    卡在中断使能里出不来；解决了中断内无法输出PWM问题

--- Sources/control.c
+++ Sources/control.c
@@ -7,62 +7,62 @@
 
 #include "control.h"
 //#include "pwm.h" 
-struct motor Motor[4];  //0->nw; 1->ne; 2->sw; 3->se
-void vertical_output(struct motor t[],uint8_t speed_y) //向前为正，向后为负
+//struct motor *Motor;  //0->nw; 1->ne; 2->sw; 3->se
+void vertical_output(Motor t[],float speed_y) //向前为正，向后为负
 {
 	int i=0;
-	t[0].duty = speed_y;
-	t[1].duty = speed_y;
-	t[2].duty = speed_y;
-	t[3].duty = speed_y;
+	t[0]->duty = speed_y;
+	t[1]->duty = speed_y;
+	t[2]->duty = speed_y;
+	t[3]->duty = speed_y;
 	for(i=0;i<4;i++)
 	{
-		if(t[i].duty>=1) t[i].duty = 1.0f;
-		if(t[i].duty<=-1) t[i].duty = -1.0f;
-		motor_output(t[i],t[i].duty);
+		if(t[i]->duty>=1) t[i]->duty = 1.0f;
+		if(t[i]->duty<=-1) t[i]->duty = -1.0f;
+		motor_output(t[i],t[i]->duty);
 	}
 }
 
-void horizontal_output(struct motor t[],uint8_t speed_x)  //向右为正，向左为负
+void horizontal_output(Motor t[],uint8_t speed_x)  //向右为正，向左为负
 {
 	int i=0;
-	t[0].duty = speed_x;
-	t[1].duty = -speed_x;
-	t[2].duty = -speed_x;
-	t[3].duty = speed_x;
+	t[0]->duty = speed_x;
+	t[1]->duty = -speed_x;
+	t[2]->duty = -speed_x;
+	t[3]->duty = speed_x;
 	for(i=0;i<4;i++)
 	{
-		if(t[i].duty>=1) t[i].duty = 1.0f;
-		if(t[i].duty<=-1) t[i].duty = -1.0f;
-		motor_output(t[i],t[i].duty);
+		if(t[i]->duty>=1) t[i]->duty = 1.0f;
+		if(t[i]->duty<=-1) t[i]->duty = -1.0f;
+		motor_output(t[i],t[i]->duty);
 	}
 }
 
-void rotating_output(struct motor t[],uint8_t angular_speed)  //顺时针为正，逆时针为负
+void rotating_output(Motor t[],uint8_t angular_speed)  //顺时针为正，逆时针为负
 {
 	int i=0;
-	t[0].duty = -angular_speed * (half_track_distance + half_wheelbase);
-	t[1].duty = angular_speed * (half_track_distance + half_wheelbase);
-	t[2].duty = -angular_speed * (half_track_distance + half_wheelbase);
-	t[3].duty = angular_speed * (half_track_distance + half_wheelbase);
+	t[0]->duty = -angular_speed * (half_track_distance + half_wheelbase);
+	t[1]->duty = angular_speed * (half_track_distance + half_wheelbase);
+	t[2]->duty = -angular_speed * (half_track_distance + half_wheelbase);
+	t[3]->duty = angular_speed * (half_track_distance + half_wheelbase);
 	for(i=0;i<4;i++)
 	{
-		if(t[i].duty>=1) t[i].duty = 1.0f;
-		if(t[i].duty<=-1) t[i].duty = -1.0f;
-		motor_output(t[i],t[i].duty);
+		if(t[i]->duty>=1) t[i]->duty = 1.0f;
+		if(t[i]->duty<=-1) t[i]->duty = -1.0f;
+		motor_output(t[i],t[i]->duty);
 	}
 }
 
-void motor_config(struct motor motor[],EMIOSn_CH forward_ch[],EMIOSn_CH backward_ch[])
+void motor_config(Motor t[],EMIOSn_CH forward_ch[],EMIOSn_CH backward_ch[])
 {
-	motor[0].backward_ch = backward_ch[0];
-	motor[0].forward_ch = forward_ch[0];
-	motor[1].backward_ch = backward_ch[1];
-	motor[1].forward_ch = forward_ch[1];
-	motor[2].backward_ch = backward_ch[2];
-	motor[2].forward_ch = forward_ch[2];
-	motor[3].backward_ch = backward_ch[3];
-	motor[3].forward_ch = forward_ch[3];
+	t[0]->backward_ch = backward_ch[0];
+	t[0]->forward_ch = forward_ch[0];
+	t[1]->backward_ch = backward_ch[1];
+	t[1]->forward_ch = forward_ch[1];
+	t[2]->backward_ch = backward_ch[2];
+	t[2]->forward_ch = forward_ch[2];
+	t[3]->backward_ch = backward_ch[3];
+	t[3]->forward_ch = forward_ch[3];
 	pwm__config(forward_ch[0]);
 	pwm__config(backward_ch[0]);
 	pwm__config(forward_ch[1]);
@@ -72,27 +72,38 @@ void motor_config(struct motor motor[],EMIOSn_CH forward_ch[],EMIOSn_CH backward
 	pwm__config(forward_ch[3]);
 	pwm__config(backward_ch[3]);
 	/**EMIOS9用于驱动板的pwm3通道输出持续高电平**/
-	pwm__config(EMIOS_CH9);
-	pwm__duty_update(EMIOS_CH9,1.0f);
+//	pwm__config(EMIOS_CH9);
+//	pwm__duty_update(EMIOS_CH9,1.0f);
 }
 
-void motor_output(struct motor motor , float duty)
+void motor_output(Motor t , float duty)
 {
 	if(duty > 0.09)
 	{
-		pwm__duty_update(motor.forward_ch,duty);
-		pwm__duty_update(motor.backward_ch,0);
+		pwm__duty_update(t->forward_ch,duty);
+		pwm__duty_update(t->backward_ch,0);
 	}
 	if(duty < -0.09)
 	{
-		pwm__duty_update(motor.forward_ch,0);
-		pwm__duty_update(motor.backward_ch,duty);
+		pwm__duty_update(t->forward_ch,0);
+		pwm__duty_update(t->backward_ch,duty);
 	}
 	else
 	{
-		pwm__duty_update(motor.backward_ch,0);
-		pwm__duty_update(motor.forward_ch,0);
+		pwm__duty_update(t->backward_ch,0);
+		pwm__duty_update(t->forward_ch,0);
 	}
 }
-
+//void Motor__config(struct motor motor,EMIOSn_CH forward_ch,EMIOSn_CH backward_ch)
+//{
+//	motor.backward_ch = backward_ch;
+//	motor.forward_ch = forward_ch;
+//	pwm__config(forward_ch);
+//	pwm__config(backward_ch);
+//	/**EMIOS9用于驱动板的pwm3通道输出持续高电平**/
+//	pwm__config(EMIOS_CH9);
+//	pwm__duty_update(EMIOS_CH9,1.0f);
+//	/***************************************/
+////	PID__config(&(motor->motor_pid), kp, ki, kd, period_ms, perror_max, ierror_max, derror_max);
+//}
 
--- Sources/main.c
+++ Sources/main.c
@@ -7,49 +7,65 @@
 #include "control.h"
 #include "electromagnet.h"
 #include "delay.h"
+#include "gpio.h"
+#include "disableWatchdog.h"
 
 extern uint8_t FLAG;
 extern uint8_t data[4];
 extern float pram[4];
-extern struct motor Motor[4];
+static Motor_t *motor[4];
 extern EMIOSn_CH forward_ch_r[4]={EMIOS_CH2,EMIOS_CH3,EMIOS_CH4,EMIOS_CH5};
 extern EMIOSn_CH backward_ch_r[4]={EMIOS_CH11,EMIOS_CH12,EMIOS_CH13,EMIOS_CH14};
 int main(void) 
 {
-	PIT__config(PIT_Timer1,10,64,test,10);
-	initLINFlex_0_UART(12);
 	initModesAndClock(); 
 	enableIrq();
-	motor_config(Motor,forward_ch_r,backward_ch_r);
+//	initLINFlex_0_UART(12);
+	motor_config(motor,forward_ch_r,backward_ch_r);
+//	Motor__config(Motor[0],EMIOS_CH5,EMIOS_CH12);
+	PIT__config(PIT_Timer1,50,64,test,10);
 //	Encoder__config(&ecd,EMIOS_CH16,0.17105,512,10,0.032,17);
 //	Encoder__init(&ecd);
-	while(1)  ;
+	GPIO__output__enable(12);
+//	SIU.GPDO[12].B.PDO =0;
+//	SIU.GPDO[14].B.PDO = 0;
+//	pwm__config(EMIOS_CH2);
+//	pwm__duty_update(EMIOS_CH2,0.5);
+	while(1)  	{
+		
+		;
+	}
+//	motor_output(Motor[0],0.8);
 }
 
 void test()
 {
+//	motor_config(motor,forward_ch_r,backward_ch_r);
 //	char temp;
-	float test_duty;
+//	float test_duty;
+//	int temp=1;
 //	PIT__config(PIT_Timer1,10,64,test,10);
-	PIT__clear_flag(PIT_Timer1);
 //	motor_config(Motor,forward_ch_r,backward_ch_r);
-	if(FLAG)
-	{
-		switch(data[3])
-		{
-		case 'A':
-			test_duty=pram[0];
-			break;
-		case 'B':
-			test_duty=-pram[0];
-			break;
-		default:
-			test_duty=0;
-		}
-		FLAG=0;
-	}
-	motor_output(Motor[0],test_duty);
-	open_elecm();
-	delay_ms(1000);
+//	if(FLAG)
+//	{
+//		switch(data[3])
+//		{
+//		case 'A':
+//			test_duty=pram[0];
+//			break;
+//		case 'B':
+//			test_duty=-pram[0];
+//			break;
+//		default:
+//			test_duty=0;
+//		}
+//		FLAG=0;
+//	}
+	motor_output(motor[0],0.5);
+//	open_elecm();
+//	delay_ms(1000);
+//	pwm__config(EMIOS_CH2);
+//	pwm__duty_update(EMIOS_CH2,0.5);
+	SIU.GPDO[12].B.PDO =!(SIU.GPDO[12].B.PDO);
+	PIT__clear_flag(PIT_Timer1);
 }
-
